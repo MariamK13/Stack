bc634cf45433c5fcd4d57d76b9b07b2cb544e304
MariamK13 <126679230+MariamK13@users.noreply.github.com>
2024-01-10 15:48:34 +0400

    Add files via upload

---
+++ Stack.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Vector"
+
+class Stack{
+	public:
+		void push_back(int pos) {
+			obj.push_back(pos);
+		}
+		void pop_back() {
+			obj.pop_back();
+		}
+		 top() {
+			return obj.
+	private:
+		Vector obj;
+};
